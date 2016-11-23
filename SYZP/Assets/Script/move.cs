83b44a8b05e733da553794b46d3c6fd70ece345c
DESKTOP-8FNOAAD\detu123 <wz@detu.com>
2016-11-23 14:09:34 +0800

    test

---
+++ SYZP/Assets/Script/move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(0, 1, 0);
+        }
+	}
+}
